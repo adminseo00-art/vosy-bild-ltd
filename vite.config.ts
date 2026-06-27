88c432ca7cc77687d12865c0aa62f90c366155e4
Nurzoda Ltd <nurzodaltd16@gmail.com>
2026-06-27 01:11:32 +0100

    Configure GitHub Pages deployment base path and update .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/vosy-bild-ltd/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
